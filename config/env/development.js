9a8eecbcc8b793eb3b79ff7bbfbc4c0084ba38b6
Johann Perozo <johann.prz@gmail.com>
2018-07-08 13:47:47 -0400

    Changes the way the db connection data is stored

--- config/env/development.js
+++ config/env/development.js
@@ -4,15 +4,10 @@ const path = require('path');
 const appRoot = require('app-root-path');
 
 const BASE_PATH = path.join(appRoot.path, 'db');
-const DB_USER = process.env.DB_USER,
-    DB_PASSWORD = process.env.DB_PASSWORD,
-    DB_ADDR = process.env.DB_ADDR || 'localhost',
-    DB_PORT = process.env.DB_PORT || 5432,
-    DB_DATABASE = process.env.DB_DATABASE;
 
 module.exports = {
     client: 'pg',
-    connection: `postgres://${DB_USER}:${DB_PASSWORD}@${DB_ADDR}:${DB_PORT}/${DB_DATABASE}`,
+    connection: 'postgres://DB_USER:DB_PASSWORD@DB_ADDR:DB_PORT/DB_DATABASE',
     migrations: {
         directory: path.join(BASE_PATH, 'migrations')
     }
